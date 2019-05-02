35c5aed32e2d86937472cd80bf34b8b2a39babbf
paulknewton <paul@topolino.org>
2019-05-03 00:38:38 +0200

    Added log_accuweather.py

---
+++ weather/log_accuweather.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+# Read current home weather conditions from accuweather (via the accuweather API)
+# and log them to my ThingSpeak.com channel
+#
+
+import json
+import requests
+
+if __name__ == '__main__':
+    # read command-line args
+    parser = argparse.ArgumentParser(
+        description="Read current home weather conditions from accuweather (via the accuweather API) and log them to my ThingSpeak.com channel")
+    parser.add_argument("thing_speak_api", help="API key to write new sensor readings to thingspeak.com channel")
+    parser.add_argument("accuweather_api", help="API key to read weather data from accuweather")
+    parser.add_argument("location_key", help="Accuweather location to retrieve weather data")
+    args = parser.parse_args()
+    print(args)
+
+    # locationKey = "1037757"  # niederdonven
+    weatherUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + args.location_key + "?apikey=" + args.accuweather_api + "&details=true"
+    weather_data = requests.get(weatherUrl).json()[0]
+
+    temperature = weather_data['Temperature']['Metric']['Value']
+    humidity = weather_data['RelativeHumidity']
+    pressure = weather_data['Pressure']['Metric']['Value']
+    rain = weather_data['Precip1hr']['Metric']['Value']
+
+    print("Temperature=%d Humidity=%d Pressure=%d Rain=%d" % (temperature, humidity, pressure, rain))
+
+    # log some data to thingspeak channel
+    requests.get(
+        "https://api.thingspeak.com/update?api_key=%s&field1=%d&field2=%d&field3=%d&field4=%d" % (
+            args.thing_speak_api, temperature, humidity, pressure, rain))
