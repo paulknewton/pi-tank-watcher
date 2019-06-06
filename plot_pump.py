104cd25122a2a517872d79aa8e35037521327984
paulknewton <paul@topolino.org>
2019-06-07 00:01:49 +0200

    Changed duration calculation to seconds

--- plot_pump.py
+++ plot_pump.py
@@ -9,16 +9,18 @@ import matplotlib
 
 
 def count_pump_on(data):
-    return len([d for d in data if d[1] == pw.PUMP_ON])
+    # cast to int in case the data is some other type
+    return len([d for d in data if int(d[1]) == pw.PUMP_ON])
 
 
 def count_pump_off(data):
-    return len([d for d in data if d[1] == pw.PUMP_OFF])
+    # cast to int in case the data is some other type
+    return len([d for d in data if int(d[1]) == pw.PUMP_OFF])
 
 
 def find_next_event(data, event_type):
     for i in range(0, len(data)):
-        if data[i][1] == event_type:
+        if int(data[i][1]) == event_type:
             return data[i:]
 
 
@@ -38,23 +40,27 @@ def get_on_off_durations(data):
     return data, off_event[0] - on_event[0]
 
 
-def get_all_on_off_durations(data):
+def get_all_durations(data):
     durations = []
 
     while data:
         data, pump_duration = get_on_off_durations(data)
         if pump_duration:
+            pump_duration = pump_duration.total_seconds()  # convert to seconds - gives clearer graphs
             durations.append(pump_duration)
 
     return durations
+
+
+def thingspeak_str2date(x):
+    return datetime.strptime(x.decode("utf-8"), "%Y-%m-%d %H:%M:%S UTC")
 
 
 def build_graphs(filename, show_graphs=False):
     """Generates graphs for pump data. Saves files as .PNG"""
     print("Reading data from %s..." % filename)
 
-    str2date = lambda x: datetime.strptime(x.decode("utf-8"), "%Y-%m-%d %H:%M:%S UTC")
-    data = genfromtxt(filename, delimiter=",", dtype=None, names=True, converters={0: str2date})
+    data = genfromtxt(filename, delimiter=",", dtype=None, names=True, converters={0: thingspeak_str2date})
     print(data.dtype.names)
 
     print("Generating graphs...")
@@ -67,26 +73,35 @@ def build_graphs(filename, show_graphs=False):
 
     time, sample_id, event = zip(*data)  # * operator to unpack to positional args --> unzip
 
+    # ---------- FIGURE ----------
     # plot raw data
     df = pd.DataFrame({"time": time, "pump": event})
     # print(df)
     df.plot(x="time", y="pump")
     plt.savefig("graphs/fig_pump.png", bbox_inches='tight')
 
-    # plot durations
-    durations = np.array(get_all_on_off_durations(list(zip(time, event))))
+    # ---------- FIGURE ----------
+    # plot durations when pump is on
+    durations = np.array(get_all_durations(list(zip(time, event))))
     print("durations: ", len(durations))
-    mean = np.mean(durations)
-    print("mean duration = ", mean)
-    df = pd.DataFrame({"pump duration": durations})
+    print("mean duration = ", np.mean(durations))
+    # print(durations)
+    df = pd.DataFrame({"on duration": durations})
     # print(df)
-    df.plot(kind="bar")
+    ax = df.plot(kind="bar")
+    ax.get_xaxis().set_ticks([])    # need to clear xticks here (cannot set in .plot function)
     plt.savefig("graphs/fig_pump_durations.png", bbox_inches='tight')
+
+    if show_graphs:
+        plt.show()
+    return
 
     std_dev = df.loc[:, "pump duration"].std()
     print("std dev = ", std_dev)
 
+    # ---------- FIGURE ----------
     # drop 1 std dev
+    mean = np.mean(durations)
     durations = durations[(durations >= mean - std_dev) & (durations <= mean + std_dev)]
     print("stripped stddev durations:", len(durations))
     print("mean duration = ", np.mean(durations))
@@ -94,6 +109,7 @@ def build_graphs(filename, show_graphs=False):
     # print(df)
     df.plot(kind="bar")
 
+    # ---------- FIGURE ----------
     # drop upper/lower percentiles
     durations = durations[(durations >= np.percentile(durations, 10)) & (durations <= np.percentile(durations, 90))]
     print("stripped percentile durations:", len(durations))
--- test_pump.py
+++ test_pump.py
@@ -1,49 +1,147 @@
 import plot_pump as pp
 import pump_watcher as pw
 import pytest
+from datetime import datetime
 
 
 @pytest.fixture
 def setup():
-    data = [(176, 1), (186, 0), (241, 1), (246, 0), (342, 1), (345, 0), (378, 1), (381, 0), (424, 1), (436, 0),
-            (505, 1), (620, 1),
-            (631, 0), (667, 1), (673, 0), (772, 1), (783, 0)]
+
+    # this is the format used by ThingSpeak
+    raw_data = [
+        ("2019-06-06 12:12:56 UTC", "1"),
+        ("2019-06-06 12:13:36 UTC", "0"),
+        ("2019-06-06 12:15:22 UTC", "0"),
+        ("2019-06-06 12:21:21 UTC", "0"),
+        ("2019-06-06 12:42:28 UTC", "1"),
+        ("2019-06-06 12:43:31 UTC", "0"),
+        ("2019-06-06 13:13:41 UTC", "1"),
+        ("2019-06-06 13:14:40 UTC", "0"),
+        ("2019-06-06 13:20:17 UTC", "0"),
+        ("2019-06-06 13:39:50 UTC", "0"),
+        ("2019-06-06 13:45:49 UTC", "0"),
+        ("2019-06-06 13:46:53 UTC", "0"),
+        ("2019-06-06 14:19:09 UTC", "1"),
+        ("2019-06-06 14:20:13 UTC", "0"),
+        ("2019-06-06 14:52:28 UTC", "0"),
+        ("2019-06-06 14:52:49 UTC", "0"),
+        ("2019-06-06 14:53:34 UTC", "1"),
+        ("2019-06-06 14:54:38 UTC", "0"),
+        ("2019-06-06 14:55:08 UTC", "0"),
+        ("2019-06-06 14:55:53 UTC", "0"),
+        ("2019-06-06 14:57:25 UTC", "0"),
+        ("2019-06-06 14:59:25 UTC", "0"),
+        ("2019-06-06 15:06:28 UTC", "0"),
+        ("2019-06-06 15:27:10 UTC", "0"),
+        ("2019-06-06 15:27:51 UTC", "0"),
+        ("2019-06-06 15:28:06 UTC", "0"),
+        ("2019-06-06 15:29:08 UTC", "1"),
+        ("2019-06-06 15:30:11 UTC", "0"),
+        ("2019-06-06 15:32:55 UTC", "0"),
+        ("2019-06-06 15:33:56 UTC", "0"),
+        ("2019-06-06 15:39:59 UTC", "0"),
+        ("2019-06-06 15:45:55 UTC", "0"),
+        ("2019-06-06 15:52:00 UTC", "0"),
+        ("2019-06-06 15:58:00 UTC", "0"),
+        ("2019-06-06 16:03:55 UTC", "0"),
+        ("2019-06-06 16:05:45 UTC", "1"),
+        ("2019-06-06 16:06:48 UTC", "0"),
+        ("2019-06-06 16:09:55 UTC", "0"),
+        ("2019-06-06 16:15:55 UTC", "0"),
+        ("2019-06-06 16:21:55 UTC", "0"),
+        ("2019-06-06 16:27:55 UTC", "0"),
+        ("2019-06-06 16:30:05 UTC", "0"),
+        ("2019-06-06 16:33:55 UTC", "0"),
+        ("2019-06-06 16:39:55 UTC", "0"),
+        ("2019-06-06 16:43:54 UTC", "1"),
+        ("2019-06-06 16:44:57 UTC", "0"),
+        ("2019-06-06 16:45:56 UTC", "0"),
+        ("2019-06-06 16:52:01 UTC", "0"),
+        ("2019-06-06 16:55:08 UTC", "0"),
+        ("2019-06-06 16:57:57 UTC", "0"),
+        ("2019-06-06 17:03:57 UTC", "0"),
+        ("2019-06-06 17:09:57 UTC", "0"),
+        ("2019-06-06 17:15:57 UTC", "0"),
+        ("2019-06-06 17:22:02 UTC", "0"),
+        ("2019-06-06 17:22:53 UTC", "1"),
+        ("2019-06-06 17:23:56 UTC", "0"),
+        ("2019-06-06 17:28:02 UTC", "0"),
+        ("2019-06-06 17:33:57 UTC", "0"),
+        ("2019-06-06 17:40:02 UTC", "0"),
+        ("2019-06-06 17:45:18 UTC", "0"),
+        ("2019-06-06 17:52:14 UTC", "0"),
+        ("2019-06-06 17:58:17 UTC", "0"),
+        ("2019-06-06 18:02:41 UTC", "1"),
+        ("2019-06-06 18:03:45 UTC", "0"),
+        ("2019-06-06 18:43:13 UTC", "1"),
+        ("2019-06-06 18:44:11 UTC", "0"),
+        ("2019-06-06 19:00:22 UTC", "0"),
+        ("2019-06-06 19:14:34 UTC", "0"),
+        ("2019-06-06 19:20:29 UTC", "0"),
+        ("2019-06-06 19:22:19 UTC", "0"),
+        ("2019-06-06 19:24:49 UTC", "1"),
+        ("2019-06-06 19:25:53 UTC", "0"),
+        ("2019-06-06 19:28:24 UTC", "0"),
+        ("2019-06-06 19:29:45 UTC", "0"),
+        ("2019-06-06 19:31:44 UTC", "0"),
+        ("2019-06-06 19:32:23 UTC", "0"),
+        ("2019-06-06 19:34:23 UTC", "0"),
+        ("2019-06-06 19:37:14 UTC", "0"),
+        ("2019-06-06 19:37:30 UTC", "0"),
+        ("2019-06-06 19:39:30 UTC", "0")
+    ]
+
+    # convert to datetime instances
+    data = []
+    for time, state in raw_data:
+        data.append((pp.thingspeak_str2date(time), state))
     return data
 
 
 def test_count_simple():
+    """Test with simple data"""
     data = [(100, 1), (101, 0), (102, 1), (103, 0)]
     assert pp.count_pump_on(data) == 2
     assert pp.count_pump_off(data) == 2
 
 
 def test_count_empty():
+    """Test with no data"""
     assert pp.count_pump_on([]) == 0
     assert pp.count_pump_off([]) == 0
 
 
-def test_count_missing(setup):
+def test_count_noisy_data(setup):
+    """Test with noisy data (ON/OFF events do not appear in clean sequence"""
     data = setup
-    assert pp.count_pump_on(data) == 9
-    assert pp.count_pump_off(data) == 8
+    assert pp.count_pump_on(data) == 12
+    assert pp.count_pump_off(data) == 68
 
 
-def test_durations(setup):
+def test_durations_simple(setup):
+    """Test when data starts cleanly with a single ON event, and ends cleanly with a singel OFF event (may include noise in between)"""
     data = setup
-    assert pp.get_all_on_off_durations(data) == [10, 5, 3, 3, 12, 126, 6, 11]
+    assert pp.get_all_durations(data[:-8]) == [40.0, 63.0, 59.0, 64.0, 64.0, 63.0, 63.0, 63.0, 63.0, 64.0, 58.0, 64.0]
 
 
-def test_durations_trailing(setup):
+def test_durations_start_with_off(setup):
+    """Test when data starts with an OFF event (instead of an ON event)"""
     data = setup
-    data.append((data[-1][0] + 1, 1))
-    assert pp.get_all_on_off_durations(data) == [10, 5, 3, 3, 12, 126, 6, 11]
+    assert pp.get_all_durations(data[1:]) == [63.0, 59.0, 64.0, 64.0, 63.0, 63.0, 63.0, 63.0, 64.0, 58.0, 64.0]
+
+
+def test_durations_end_with_multiple_off(setup):
+    """Test when data ends with multiple repeating OFF events"""
+    data = setup
+    assert pp.get_all_durations(data) == [40.0, 63.0, 59.0, 64.0, 64.0, 63.0, 63.0, 63.0, 63.0, 64.0, 58.0, 64.0]
 
 
 def test_durations_missing_on():
+    """Test with no ON events at all"""
     data = [(176, 0), (186, 0), (241, 0), (246, 0), (342, 0), (345, 0), (378, 0), (381, 0), (424, 0), (436, 0),
             (505, 0), (620, 0),
             (631, 0), (667, 0), (673, 0), (772, 0), (783, 0)]
-    assert not pp.get_all_on_off_durations(data)
+    assert not pp.get_all_durations(data)
 
 
 def test_data_generation():
@@ -51,7 +149,8 @@ def test_data_generation():
 
 
 def test_callbacks():
-    pump = pw.SumpPump(9, test_mode=True)   # setup pump on PIN 9
+    """Test that callbacks can be added to a pump and these are triggered by events"""
+    pump = pw.SumpPump(9, test_mode=True)  # setup pump on PIN 9
 
     # dummy logger to record events
     class DummyLogger:
@@ -66,8 +165,9 @@ def test_callbacks():
 
     # simulate an event on PIN 9
     pump.event(9)
-    assert len(test_logger1.events) == 1     # check only 1 event
-    assert test_logger1.events == [[-1]]    # Loggers log a list of fields, even if only 1 entry. -1 is the default value if GPIO not used
+    assert len(test_logger1.events) == 1  # check only 1 event
+    assert test_logger1.events == [
+        [-1]]  # Loggers log a list of fields, even if only 1 entry. -1 is the default value if GPIO not used
 
     # add 2 more listeners
     test_logger2 = DummyLogger()
@@ -77,7 +177,7 @@ def test_callbacks():
 
     # simulate an event on PIN 9
     pump.event(9)
-    assert len(test_logger1.events) == 2     # check 2 events
+    assert len(test_logger1.events) == 2  # check 2 events
     assert test_logger1.events == [[-1], [-1]]
     assert len(test_logger2.events) == 1
     assert test_logger2.events == [[-1]]
