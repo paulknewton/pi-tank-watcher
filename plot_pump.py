a695e5ebcd1aee53b0a27fed1dd72a10bdb2bf4a
paulknewton <paul@topolino.org>
2019-08-09 23:09:34 +0200

    QA fixes

--- plot_pump.py
+++ plot_pump.py
@@ -23,6 +23,7 @@ def find_next_event(data, event_type):
     for i, event in enumerate(data):
         if int(event[2]) == event_type:
             return data[i:]
+    return None
 
 
 def find_next_duration(data, event1, event2):
@@ -88,7 +89,7 @@ def plot_durations(df):
     # mark mean as horizontal line
     mean = df.iloc[:, 0].mean()
     ax.axhline(mean, color='r', linestyle="dashed")
-    #ax.text(0, mean, "mean = %s" % str(timedelta(seconds=int(mean))))
+    # ax.text(0, mean, "mean = %s" % str(timedelta(seconds=int(mean))))
 
     # mark last value
     last = df.iloc[-1].iat[0]
